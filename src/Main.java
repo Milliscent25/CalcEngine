fb5412f93d5946bf2921c47520c2d857e5fac858
Milliscent25 <milliscent@uncommon.org>
2023-08-31 10:44:50 +0200

    Switch-statement and conditional logic

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,41 @@ public class Main {
         double result = 0.0d;
         char opCode = 'd';
 
-        if(opCode == 'a')
-            result = value1 + value2;
-        else if(opCode == 's')
-            result = value1 - value2;
-        else if(opCode == 'm')
-            result = value1 * value2;
-        else if(opCode == 'd'){
-            if (value2 != 0)
-                result = value1 / value2;
-        }
-
-        else {
+        switch (opCode){
+            case 'a' :
+                result = value1 + value2;
+                break;
+            case 's' :
+                result = value1 - value2;
+                break;
+            case 'm' :
+                result = value1 * value2;
+                break;
+            case 'd' :
+                result = value2 != 0 ? value1 / value2 : 0.0d;
+//                if(value2 != 0)
+//                result = value1 / value2;
+                break;
+            default:
             System.out.println("Invalid opCode: " + opCode );
             result = 0.0d;
+
         }
+//        if(opCode == 'a')
+//            result = value1 + value2;
+//        else if(opCode == 's')
+//            result = value1 - value2;
+//        else if(opCode == 'm')
+//            result = value1 * value2;
+//        else if(opCode == 'd'){
+//            if (value2 != 0)
+//                result = value1 / value2;
+//        }
+//
+//        else {
+//            System.out.println("Invalid opCode: " + opCode );
+//            result = 0.0d;
+//        }
 
         System.out.println(result);
     }

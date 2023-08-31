7abb722598ca8af4a48e0d7c137a12b9c11acb56
Milliscent25 <milliscent@uncommon.org>
2023-08-31 14:56:42 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,97 @@
 
 public class Main {
     public static void main(String[] args) {
-        double value1 = 100.0d;
-        double value2 = 0.0d;
-        double result = 0.0d;
-        char opCode = 'd';
+        double[] leftVals = {100.d, 25.0, 225.0d,11.0d};
+        double[] rightVals = {50.0d, 92.0, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
+//        array variables
 
-        switch (opCode){
-            case 'a' :
-                result = value1 + value2;
+//        double value1 = 100.0d;
+//        double value2 = 0.0d;
+//        double result = 0.0d;
+//        char opCode[i] = 'd';
+
+        if(args.length == 0) {
+            for (int i = 0; i < opCodes.length; i++) {
+
+                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+            }
+            for (double currentResult : results)
+
+                System.out.println(currentResult);
+        } else if(args.length == 3)
+            handleCommandline(args);
+        else
+            System.out.println("Please provide an operation code and 2 numeric values");
+    }
+
+    private static void handleCommandline(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
+
+
+
+    }
+/* execute method knows how to do the work and perform an operation */
+    static double execute(char opCode, double leftVal, double rightVal){
+        double result;
+
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
                 break;
-            case 's' :
-                result = value1 - value2;
+            case 's':
+                result = leftVal - rightVal;
                 break;
-            case 'm' :
-                result = value1 * value2;
+            case 'm':
+                result = leftVal * rightVal;
                 break;
-            case 'd' :
-                result = value2 != 0 ? value1 / value2 : 0.0d;
+            case 'd':
+                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
 //                if(value2 != 0)
-//                result = value1 / value2;
-                break;
-            default:
-            System.out.println("Invalid opCode: " + opCode );
-            result = 0.0d;
+//                    result = value1 / value2;
 
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
         }
-//        if(opCode == 'a')
+       return result;
+    }
+
+}
+
+
+/*           switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
+               if(value2 != 0)
+               result = value1 / value2;
+                    break;
+                default:
+                    System.out.println("Invalid opCode: " + opCodes[i]);
+                    results[i] = 0.0d;
+                    break;
+            }
+
+  */
+
+
+//  if(opCode == 'a')
 //            result = value1 + value2;
 //        else if(opCode == 's')
 //            result = value1 - value2;
@@ -41,58 +106,6 @@ public class Main {
 //            System.out.println("Invalid opCode: " + opCode );
 //            result = 0.0d;
 //        }
-
-        System.out.println(result);
-    }
-
-
-
-}
-//      double value1;
-//      double value2;
-//      double result;
-//      char opCode = 0;
-//
-//      switch (opCode == 'a'){
-//          result = value1 + value2;
-//
-//          case (opCode == 's'):
-//           result = value1 - value2;
-//        break;
-//          case (opCode == 'm'):
-//            result = value1 * value2;
-//              break;
-//          case (opCode == 'd'):
-//              result = value1 / value2;
-//              break;
-//          case
-//          result = 0.0d;
-//
-//          System.out.println(result);
-//        }
-//        switch {
-//            case : opCode = 'a';
-//                result = value1 + value2;
-//                break;
-//            case : opCode = 's';
-//                System.out.println("value1 - value2;");
-//                break;
-//            case : opCode = 'a';
-//                System.out.println("value1 * value2");
-//            case : opCode = 'a';
-//                System.out.println("value1 / value2");
-//                break;
-//            default :
-//                System.out.println("0.0d");
-//        }
-//        System.out.println(" result ");
-//
-//        int a = 10;
-//        while (a >= 1){
-//            System.out.println(a);
-//            a--;
-//        }
-
 
         /* Arrays*/
 //        int[] array = new int[]{1, 2 , 3, 4 };
